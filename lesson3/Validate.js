80a74d8351b678f78d041178a655f5a2428a55ad
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2019-01-11 21:51:46 +1000

    validating continues

--- lesson3/Validate.js
+++ lesson3/Validate.js
@@ -1,25 +1,41 @@
 class Validate {
-    constructor(currentElement) {
-        this.el = currentElement;
+    constructor(currentElement, errorClass = "error") {
+        this.el = null;
         this.id = currentElement.id;
         this.name = currentElement.name;
         this.value = currentElement.value;
+        this.errorId = currentElement.dataset.errorid;
+        this.regExp = '';
+        this.errorClass = errorClass;
+        this._init();
         this.toCheck();
     }
 
-    toCheck(value) {
-        this._init();
-        this._isValid();
-
-    }
-    _init(){
-        console.log(this.el);
-        // thit.id = _getID();
+    _init() {
+        this.el = document.getElementById(this.id);
+        this.regExp = this._getRegExp(this.name);
+        document.getElementById(this.errorId).classList.add('hide');
     }
 
-    _isValid(fieldType, value) {
-        if (fieldType === "name") {
-
+    toCheck() {
+        if (!this._isValid(this.regExp, this.value)) {
+            this.el.classList.add(this.errorClass);
+            let errorEl = document.getElementById(this.errorId)
+            errorEl.classList.remove('hide');
+        } else {
+            this.el.classList.remove(this.errorClass);
         }
+    }
+
+    _getRegExp(name) {
+        if (name === 'name') {
+            return /^[A-zА-яЁё]+$/
+        } else {
+            return /\d/;
+        }
+    }
+
+    _isValid(regExp, value) {
+        return regExp.exec(value);
     }
 }
